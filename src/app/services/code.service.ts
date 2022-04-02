322799fb8ee56c1835364c3bda46ca15afce9aed
Gabriel Corrêa <gabriel@correa.dev.br>
2022-04-02 10:24:44 -0300

    fix: example code url

--- src/app/services/code.service.ts
+++ src/app/services/code.service.ts
@@ -1,23 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CodeService {
+  apiLink =
+    'https://raw.githubusercontent.com/biel-correa/prova-estrutura-de-dados/main/src/assets/code/';
   constructor(private http: HttpClient) {}
 
   getFilaCode() {
-    return this.http.get('assets/code/fila.py', { responseType: 'text' });
+    return this.http.get(`${this.apiLink}/fila.py`, { responseType: 'text' });
   }
 
   getPilhaCode() {
-    return this.http.get('assets/code/pilha.py', { responseType: 'text' });
+    return this.http.get(`${this.apiLink}/pilha.py`, { responseType: 'text' });
   }
 
   getFilaCircularCode() {
-    return this.http.get('assets/code/filaCircular.py', {
+    return this.http.get(`${this.apiLink}/filaCircular.py`, {
       responseType: 'text',
     });
   }
